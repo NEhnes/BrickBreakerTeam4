2c1082a9c025adbf4db4437c8c9f5e3c9d1c8f9e
NEhnes <162652120+NEhnes@users.noreply.github.com>
2025-05-16 14:46:09 -0400

    started powerup collision method

--- BrickBreaker/Ball.cs
+++ BrickBreaker/Ball.cs
@@ -172,6 +172,10 @@ namespace BrickBreaker
             return didCollide;
         }
 
+        public void PowerupCollision()
+        {
+        }
+
         public static double Map (double value, double fromMin, double fromMax, double newMin, double newMax)
         {
             return (value - fromMin) * (newMax - newMin) / (fromMax - fromMin) + newMin;
